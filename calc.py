3cc6cf52ba09cb88c854ad8bc7f04bbddc9645df
Cameron <camy_m_do@hotmail.com>
2021-10-11 11:49:12 +1100

    Add comments to use-unpacking version

--- calc.py
+++ calc.py
@@ -1,7 +1,6 @@
 def compute(expression):
     num0, operator, num1 = expression.split(' ')
     num0, num1 = int(num0), int(num1)
-
     if operator == '+':
         return num0 + num1
     elif operator == '-':
@@ -9,3 +8,4 @@ def compute(expression):
     else:
         print('unknown operator!')
         return None
+#some more comments from use-unpacking
